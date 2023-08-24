0b9172b1196d8af14be748fe8941be799487664d
cjhw <1784581164@qq.com>
2023-08-24 20:12:32 +0800

    feat: render jsx to client

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ createServer(async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     if (url.pathname === "/client.js") {
       await sendScript(res, "./client.js");
+    } else if (url.searchParams.has("jsx")) {
+      url.searchParams.delete("jsx");
+      await sendJSX(res, <Router url={url} />);
     } else {
       await sendHTML(res, <Router url={url} />);
     }
@@ -107,6 +110,13 @@ async function sendScript(res, filename) {
   res.end(content);
 }
 
+async function sendJSX(res, jsx) {
+  const clientJSX = await renderJSXToClientJSX(jsx);
+  const clientJSXString = JSON.stringify(clientJSX, null, 2);
+  res.setHeader("Content-Type", "application/json");
+  res.end(clientJSXString);
+}
+
 async function sendHTML(res, jsx) {
   let html = await renderJSXToHTML(jsx);
   html += `<script type="module" src="/client.js"></script>`;
@@ -118,6 +128,44 @@ function throwNotFound(cause) {
   const notFound = new Error("Not found.", { cause });
   notFound.statusCode = 404;
   throw notFound;
+}
+
+async function renderJSXToClientJSX(jsx) {
+  console.log(jsx);
+  if (
+    typeof jsx === "string" ||
+    typeof jsx === "number" ||
+    typeof jsx === "boolean" ||
+    jsx == null
+  ) {
+    return jsx;
+  } else if (Array.isArray(jsx)) {
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX(child)));
+  } else if (jsx != null && typeof jsx === "object") {
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      if (typeof jsx.type === "string") {
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        };
+      } else if (typeof jsx.type === "function") {
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = await Component(props);
+        return renderJSXToClientJSX(returnedJsx);
+      } else throw new Error("Not implemented.");
+    } else {
+      console.log(jsx);
+      return Object.fromEntries(
+        await Promise.all(
+          Object.entries(jsx).map(async ([propName, value]) => [
+            propName,
+            await renderJSXToClientJSX(value),
+          ])
+        )
+      );
+    }
+  } else throw new Error("Not implemented");
 }
 
 async function renderJSXToHTML(jsx) {
